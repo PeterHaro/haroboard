7ea3c1f14c18ca74de8f07a9162b341e403108c3
Peter Haro <Peter Haro>
2017-02-08 15:51:40 +0100

    alot of junk that needs to be cleaned up when I get the time

--- public/libs/haroBoard/maps/Cesium/haroboard/cesium_utility.js
+++ public/libs/haroBoard/maps/Cesium/haroboard/cesium_utility.js
@@ -1,3 +1,39 @@
+function CesiumUtility() {
+    var defaultKMLEyeOffset = new Cesium.Cartesian3(0.0, 5000.0, 0.0);
+    var defaultScaleByDistance = new Cesium.NearFarScalar(1, 0.5, 1, 0.3);
+    var defaultTranslucency = new Cesium.NearFarScalar(1.5e2, 1, 3.0e6, 0);
+}
+
+CesiumUtility.prototype.LoadWms = function(layerId, geoDataSrc, geoLayers, noFeatures) {
+    var src;
+    if (noFeatures) {
+        src = viewer.imageryLayers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
+            url: geoDataSrc,
+            layers: geoLayers,
+            sourceUri: geoDataSrc,
+            enablePickFeatures: false,
+            tilingScheme: new Cesium.WebMercatorTilingScheme(),
+            parameters: {
+                transparent: true,
+                format: 'image/png' //TODO: Fetch from config
+            }
+        }));
+    } else {
+        src = viewer.imageryLayers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
+            url: geoDataSrc,
+            layers: geoLayers,
+            sourceUri: geoDataSrc,
+            tilingScheme: new Cesium.WebMercatorTilingScheme(),
+            parameters: {
+                transparent: true,
+                format: 'image/png' //TODO: Fetch from config
+            }
+        }));
+    }
+    activeLayers[layerId] = src;
+    loadSliders(src, layerId);
+};
+
 function CesiumBaseLayerInjection() {
     this._imageryViewModels = [];
     this._terrainProviders = [];
